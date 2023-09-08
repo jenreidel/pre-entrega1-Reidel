f190fc84f31680f89ed2a49bf4635f8bb6b5a489
Jenisse Reidel <jeyula.lp@gmail.com>
2023-09-08 04:28:01 -0300

    agrego navbar y su hoja de estilo y borro lo que habia en los css de App y de index

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
 
 function App() {
 
   return (
-    <>
-      
-    </>
-  )
-}
+    <main>
+      <NavBar />
+    </main>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <img src="" alt="" />
+        </div>
+    );
+};
+
+export default NavBar;
